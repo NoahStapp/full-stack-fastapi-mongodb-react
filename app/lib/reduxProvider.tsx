6fa9c27c0794e709256bbf6107c3d10dd2674ef1
Noah Stapp <noah.stapp@mongodb.com>
2023-10-05 12:55:18 -0700

    Cleanup

--- app/lib/reduxProvider.tsx
+++ app/lib/reduxProvider.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import { store } from "./lib/store";
+import { store } from "./store";
 import { Provider } from "react-redux";
 
 export const ReduxProvider = (props: React.PropsWithChildren) => {
